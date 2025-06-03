4d76350eec4f7b338550ef3b33fb33b26d04a284
Youssef-Mhmoud <joshuman526@gmail.com>
2025-06-03 16:12:20 +0300

    style hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
-    <div> </div>
-  )
-}
+    <div>
+      <Hero />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
