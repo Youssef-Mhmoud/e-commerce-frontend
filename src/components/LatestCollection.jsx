11419e879db280ea2017fd4682bd4de04095f58a
Youssef-Mhmoud <joshuman526@gmail.com>
2025-06-03 17:53:38 +0300

    make a title component

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+// import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
 
 const LatestCollection = () => {
-  return (
-    <div>LatestCollection</div>
-  )
-}
+  // const { products } = useContext(ShopContext);
 
-export default LatestCollection
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title titleText_1="LATEST" titleText_2="COLLECTION" />
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollection;
---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+const Title = ({ titleText_1, titleText_2 }) => {
+  return (
+    <div className="inline-flex gap-2 items-center mb-3">
+      <p className="text-gray-500">
+        {titleText_1}{" "}
+        <span className="font-medium text-gray-700">{titleText_2}</span>
+      </p>
+      <p className="w-8 sm:w-11 h-[1px] sm:h-[2px] bg-gray-700"></p>
+    </div>
+  );
+};
+
+export default Title;
