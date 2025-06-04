378c41b58037882f6bab2d16e1e55f56bbead2b6
Youssef-Mhmoud <joshuman526@gmail.com>
2025-06-04 19:09:05 +0300

    fix navbar active class

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img
             src={assets.cart_icon}
             alt="Cart icon"
-            className="w-5 cursor-pointer min-w-5"
+            className="w-5 cursor-pointer"
           />
           <p className="absolute bg-black text-white rounded-full w-4 leading-4 text-center text-[8px] left-2 top-3 aspect-square">
             0
