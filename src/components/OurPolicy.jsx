37a48e6f0f8e4c5a51ae01f4504aed802db4ef57
Youssef-Mhmoud <joshuman526@gmail.com>
2025-06-08 11:08:23 +0300

    add our policy component

---
+++ src/components/OurPolicy.jsx
@@ -0,0 +1,15 @@
+import { assets } from "../assets/frontend_assets/assets";
+
+const OurPolicy = () => {
+  return (
+    <div>
+      <div>
+        <img src={assets.exchange_icon} alt="Exchange icon" />
+        <p>Easy Exchange Policy</p>
+        <p>We offer hassle free exchange policy</p>
+      </div>
+    </div>
+  );
+};
+
+export default OurPolicy;
