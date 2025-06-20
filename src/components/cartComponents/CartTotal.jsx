e349efb04c0b1e56c6f0a25f8e0a9b62f1e879e6
Youssef-Mhmoud <joshuman526@gmail.com>
2025-06-20 09:03:19 +0300

    add cart total component

---
+++ src/components/cartComponents/CartTotal.jsx
@@ -0,0 +1,30 @@
+import Title from "../Title";
+
+const CartTotal = ({ currency }) => {
+  return (
+    <div className="flex flex-col items-end my-10">
+      <div className="w-full sm:w-1/2">
+        <div className="text-2xl mb-[-30px]">
+          <Title titleText_1="CART" titleText_2="TOTALS" />
+        </div>
+        <div className="flex justify-between border-b border-gray-200 py-2 text-sm">
+          <p>Subtotal</p>
+          <p>{currency}</p>
+        </div>
+        <div className="flex justify-between border-b border-gray-200 py-2 text-sm">
+          <p>Shipping Fee</p>
+          <p>{currency}</p>
+        </div>
+        <div className="flex justify-between py-2 text-sm">
+          <p className="font-semibold">Total</p>
+          <p>{currency}</p>
+        </div>
+        <button className="bg-black text-white py-3 px-8 text-sm block ml-auto mt-5 cursor-pointer hover:opacity-80 duration-300">
+          PROCEED TO CHECKOUT
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartTotal;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { CartContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import ProductCart from "../components/cartComponents/ProductCart";
+import CartTotal from "../components/cartComponents/CartTotal";
 
 const Cart = () => {
   const { products, currency, cartItems } = useContext(CartContext);
@@ -31,26 +32,7 @@ const Cart = () => {
           <div className="border-t border-b border-gray-200 text-gray-700">
             <ProductCart cartData={cartData} currency={currency} />
           </div>
-          <div className="flex flex-col items-end my-10">
-            <div className="w-full sm:w-1/2">
-              <div className="text-2xl mb-[-30px]">
-                <Title titleText_1="CART" titleText_2="TOTALS" />
-              </div>
-              <div className="flex justify-between border-b border-gray-200 py-2 text-sm">
-                <p>Subtotal</p>
-                <p>{currency}</p>
-              </div>
-              <div className="flex justify-between border-b border-gray-200 py-2 text-sm">
-                <p>Shipping Fee</p>
-                <p>{currency}</p>
-              </div>
-              <div className="flex justify-between py-2 text-sm">
-                <p className="font-semibold">Total</p>
-                <p>{currency}</p>
-              </div>
-              <button className="bg-black text-white py-3 px-8 text-sm block ml-auto mt-5">PROCEED TO CHECKOUT</button>
-            </div>
-          </div>
+          <CartTotal currency={currency} />
         </>
       ) : (
         <p className="text-center py-10 text-gray-500">Your cart is empty.</p>
