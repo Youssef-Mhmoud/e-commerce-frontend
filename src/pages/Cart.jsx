30ccb94359ca1889e4bb44825949c8459eb1739a
Youssef-Mhmoud <joshuman526@gmail.com>
2025-06-20 09:00:37 +0300

    style cart Totals in cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -22,14 +22,36 @@ const Cart = () => {
 
   return (
     <div className="border-top pt-6">
-      <div className="text-2xl">
+      <div className="text-2xl mb-[-20px]">
         <Title titleText_1="YOUR" titleText_2="CART" />
       </div>
       {/* Product */}
       {cartData.length > 0 ? (
-        <div className="border-t border-b border-gray-200 text-gray-700">
-          <ProductCart cartData={cartData} currency={currency} />
-        </div>
+        <>
+          <div className="border-t border-b border-gray-200 text-gray-700">
+            <ProductCart cartData={cartData} currency={currency} />
+          </div>
+          <div className="flex flex-col items-end my-10">
+            <div className="w-full sm:w-1/2">
+              <div className="text-2xl mb-[-30px]">
+                <Title titleText_1="CART" titleText_2="TOTALS" />
+              </div>
+              <div className="flex justify-between border-b border-gray-200 py-2 text-sm">
+                <p>Subtotal</p>
+                <p>{currency}</p>
+              </div>
+              <div className="flex justify-between border-b border-gray-200 py-2 text-sm">
+                <p>Shipping Fee</p>
+                <p>{currency}</p>
+              </div>
+              <div className="flex justify-between py-2 text-sm">
+                <p className="font-semibold">Total</p>
+                <p>{currency}</p>
+              </div>
+              <button className="bg-black text-white py-3 px-8 text-sm block ml-auto mt-5">PROCEED TO CHECKOUT</button>
+            </div>
+          </div>
+        </>
       ) : (
         <p className="text-center py-10 text-gray-500">Your cart is empty.</p>
       )}
