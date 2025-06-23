7f8687497a20c685d0c818d5917dfb5e88745070
Youssef-Mhmoud <joshuman526@gmail.com>
2025-06-23 13:45:17 +0300

    style place order page

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,8 +1,51 @@
+import { assets } from "../assets/frontend_assets/assets";
+import CartTotal from "../components/cartComponents/CartTotal";
+import Title from "../components/Title";
 
 const PlaceOrder = () => {
   return (
-    <div>PlaceOrder</div>
-  )
-}
+    <div className="flex">
+      <form>
+        <div>
+          <input type="text" placeholder="First name" required />
+          <input type="text" placeholder="Last name" required />
+        </div>
+        <div>
+          <input type="email" placeholder="Email address" required />
+          <input type="text" placeholder="Street" required />
+        </div>
+        <div>
+          <input type="text" placeholder="City" required />
+          <input type="text" placeholder="State" required />
+        </div>
+        <div>
+          <input type="text" placeholder="Zipcode" required />
+          <input type="text" placeholder="Country" required />
+        </div>
+        <input type="tel" placeholder="Phone" required />
+      </form>
+      <div>
+        <div>
+          <CartTotal />
+        </div>
+        <div>
+          <Title titleText_1="PAYMENT" titleText_2="METHOD" />
+          <div>
+            <input type="checkbox" />
+            <img src={assets.stripe_logo} alt="Stripe payment method" />
+          </div>
+          <div>
+            <input type="checkbox" />
+            <img src={assets.stripe_logo} alt="Stripe payment method" />
+          </div>
+          <div>
+            <input type="checkbox" />
+            <img src={assets.stripe_logo} alt="Stripe payment method" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default PlaceOrder
+export default PlaceOrder;
